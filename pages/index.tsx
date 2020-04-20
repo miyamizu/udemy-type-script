a14d7d345fb770c3a816250bc01e0c9f19a75eef
miyazaki <kattyan.08ms@gmail.com>
2020-04-20 10:26:59 +0900

    26. Higher Order Components React Redux

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,12 @@
-import React from 'react';
+import { Provider } from 'react-redux';
+import { reducer } from '../reducer';
+import { createStore } from 'redux';
+import { ConnectedTaskList } from '../components/TaskList';
 
-interface Task {title: string;}
+const store = createStore(reducer);
 
-interface TaskListProps {
-  initialTasks: Task[];
-}
-
-interface TasksListState {
-  tasks: Task[];
-}
-
-class TasksList extends React.Component<TaskListProps, TasksListState> {
-  constructor(props: TaskListProps) {
-    super(props);
-    this.state = {
-      tasks: props.initialTasks
-    };
-
-    this.onAddNewTaskClick = this.onAddNewTaskClick.bind(this);
-  }
-
-  onAddNewTaskClick() {
-    this.setState({tasks: [...this.state.tasks, {title: 'New Title'}]});
-  }
-
-  render() {
-    const { tasks } = this.state;
-
-    return (
-        <>
-        <ul>
-          {tasks.map((task, i) => {
-            return <li key={i}>{task.title}</li>;
-          })}
-        </ul>
-        <button onClick={this.onAddNewTaskClick}>Add new task</button>
-      </>
-    );
-  }
-}
-
-const tasks = [
-  {title: 'Task One'},
-  {title: 'Task Two'}
-];
-
-
-export default () => <div><TasksList initialTasks={tasks} /></div>;
+export default () => (
+  <Provider store={store}>
+    <ConnectedTaskList />
+  </Provider>
+);
